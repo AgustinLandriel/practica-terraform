9cbe1a65a0a852a6e60df6278392c49a6e4986df
Agustin Landriel <agusrojo20@hotmail.com>
2026-06-25 19:10:02 -0300

    Se configuro el backend S3 en AWS

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,30 +1,53 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 6.29"
-#     }
-#   }
-# }
+#Provider configuration for AWS
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.29"
+    }
+  }
+}
 
 
-# provider "aws" {
-#   region     = "us-east-2"
-#   access_key = var.access_key
-#   secret_key = var.secret_key
-# }
+provider "aws" {
+  region = "us-east-2"
+}
 
 
-# resource "aws_s3_bucket" "tf_state" {
-#   bucket = "tf-state-lab"
-#   acl    = "private"
+## Creacion del bucket S3 para almacenar el estado de terraform
 
-#   versioning {
-#     enabled = true
-#   }
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "terraform-state-chucho"
+}
 
-#   tags = {
-#     Name        = "Terraform State Bucket"
-#     Project = "Laboratorio"
-#   }
-# }
+#Recurso para habilitar el versionamiento del bucket S3
+resource "aws_s3_bucket_versioning" "tf_state" {
+
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+
+#Recurso para habilitar el cifrado del bucket S3
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+#Recurso para habilitar el bloqueo de acceso público del bucket S3
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket                  = aws_s3_bucket.tf_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -1,15 +1,23 @@
 terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.29"
     }
   }
+
+  backend "s3" {
+    bucket       = "terraform-state-chucho" # el mismo nombre del bucket
+    key          = "prod/terraform.tfstate" # ← ruta del state DE DEV
+    region       = "us-east-2"
+    encrypt      = true
+    use_lockfile = true # locking nativo de S3
+  }
 }
 
 
 provider "aws" {
-  region     = "us-east-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region = "us-east-2"
 }
+
